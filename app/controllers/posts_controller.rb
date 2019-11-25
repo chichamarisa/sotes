cf26f1b3641887e2f3de7ad4756de76accc6df16
mark swaringen <mswaringen@gmail.com>
2019-11-25 11:24:10 +0100

    refactored routes and CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -14,25 +15,30 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.save
+
+    redirect_to post_path(@post)
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
+
+    redirect_to post_path(@post)
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
 
     redirect_to posts_path
   end
 
   private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:post)
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  get "posts", to: "posts#index"
-  get "posts/new", to: "posts#new"
-  get "posts/:id", to: "posts#show"
-  post "posts", to: "posts#create"
-  get "posts/:id/edit", to: "posts#edit"
-  patch "posts/:id", to: "posts#update"
-  delete "posts/:id", to: "posts#destroy"
+  resources :posts
 end
