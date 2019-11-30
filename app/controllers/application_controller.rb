011b0e4024e11f8f713e2326312db69cfbcdd506
mark swaringen <mswaringen@gmail.com>
2019-11-30 10:09:53 -0600

    login works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+protected
+
+  def after_sign_in_path_for(resource)
+    # return the path based on resource
+    posts_path
+  end
+
+  def after_sign_out_path_for(resource)
+    # return the path based on resource
+    root_path
+  end
 end
